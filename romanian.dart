25dc87c847dd809985a74ba2db7095c267c5fe59
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2019-03-16 09:46:16 +0200

    Update romanian.dart

--- romanian.dart
+++ romanian.dart
@@ -11,10 +11,10 @@ final Map<String, String> romanian = {
 
   "setupCreateTitle": "Crearea portofelului DigiID",
   "setupCreateWarn":
-      "Scrie pe o foaie.\nNu a se salva pe niciun dispozitiv electronic:",
+      "Scrie pe o foaie.\nA se nu se salva pe niciun dispozitiv electronic:",
   "setupCreateInstructions": "Apasă pe cuvinte în ordinea care au apărut:",
   "setupCreateButtonWaver":
-      "Am salvat (offline) în siguranță propoziția seed într-un loc protejat",
+      "Am salvat offline și în siguranță propoziția seed într-un loc protejat",
 
   "setupRestoreTitle": "DigiID Recuperarea portofelului",
   "setupRestoreInstructions": "Copiază propoziția mnemonic aici:",
@@ -33,21 +33,21 @@ final Map<String, String> romanian = {
   "digiidConfirmInstructions": "Introduce PIN-ul dacă este cerut",
   "digiidConfirmWaiting": "Negociind cu site-ul",
   "digiidConfirmBackupInstructions":
-      "Aceasta este recuperarea parolei tale.\nNu însetați spații.\n\n",
+      "Aceasta este metoda de recuperare a parolei tale.\nNu adăugați spații.\n\n",
 
   "digipassTitle": "DigiPassword Generator",
   "digipassSiteHint": "Domeniul site-ului",
   "digipassInstructionAbove": "Introduce domeniul mai sus, iar după care PIN-ul:",
   "digipassInstructionBelow": "Introduce domeniul mai jos, iar după care PIN-ul:",
   "digipassNote":
-      "*Bazându-ne pe seed-ul contului tău și date introduse mai sus, o să fie generată o parola unică. Dacă data viitoare vei insera același date, o să fie generată aceeași parolă.",
+      "*Bazându-ne pe seed-ul contului tău și datele introduse mai sus, o să fie generată o parola unică. Dacă data viitoare vei insera același date, o să fie generată aceeași parolă.",
   "digipassPasswordCopy": "Parola ta (apasă pentru a copia):",
 
   "warningInvalidChecksum": "Checksum invalid!  Ești sigur că este corect?",
   "warningShortMnemonic1": "Doar propoziția mnemonic ",
 //$enteredBitLength
   "warningShortMnemonic2":
-      " bits. Propoziții mnemonice mai scurte de 128bits nu sunt recomandate.",
+      " bits. Propozițiile mnemonice mai scurte de 128bits nu sunt recomandate.",
 
   "errorNoCamera": "Fără permisiuni de access la camera foto!",
   "errorUnknown": "Eroare necunoscută: ",

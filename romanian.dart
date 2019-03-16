d1199b7c492d209356c4afda11a495e568345638
Mutu Adi-Marian <39502043+Xxshark888xX@users.noreply.github.com>
2019-03-16 08:47:53 +0000

    Romanian translation (#1)
    * Create romanian.dart
    
    V0.1.3+2
    
    * Update romanian.dart
    
    V0.1.3+2
    
    * Update romanian.dart
    
    * Update romanian.dart
    
    * Update romanian.dart

---
+++ romanian.dart
@@ -0,0 +1,81 @@
+final Map<String, String> romanian = {
+  "language": "Română",
+  "languageCode": "ro",
+  "languageMnemonic": "english", //options: english,french,italian,spanish
+
+  "setupTitle": "Setări DigiID:",
+  "setupWarn":
+      "Înainte de a continua, este foarte important să te asiguri că te afli într-o locație în siguranță fără oameni și camere de supraveghere în jurul tău.",
+  "setupButtonCreate": "Creează un nou portofel",
+  "setupButtonRecover": "Restabilește un portofel deja existent",
+
+  "setupCreateTitle": "Crearea portofelului DigiID",
+  "setupCreateWarn":
+      "Scrie pe o foaie.\nA se nu se salva pe niciun dispozitiv electronic:",
+  "setupCreateInstructions": "Apasă pe cuvinte în ordinea care au apărut:",
+  "setupCreateButtonWaver":
+      "Am salvat offline și în siguranță propoziția seed într-un loc protejat",
+
+  "setupRestoreTitle": "DigiID Recuperarea portofelului",
+  "setupRestoreInstructions": "Copiază propoziția mnemonic aici:",
+
+  "setupSaveTitle": "DigiID Protecție prin PIN",
+  "setupSaveSetPin": "Alege un PIN:",
+  "setupSaveSaving":  "Salvarea seed-ului",
+  "setupSavePinNote": "*Numere schimbate pentru a evita regăsirea amprentelor.",
+
+  "settingTitle": "Setări",
+  "settingAllowBiometric": "Permite Biometric:",
+  "settingStyle": "Tip:",
+  "settingLanguage": "Limba:",
+
+  "digiidConfirmTitle": "Confirmă domeniul",
+  "digiidConfirmInstructions": "Introduce PIN-ul dacă este cerut",
+  "digiidConfirmWaiting": "Negociind cu site-ul",
+  "digiidConfirmBackupInstructions":
+      "Aceasta este metoda de recuperare a parolei tale.\nNu adăugați spații.\n\n",
+
+  "digipassTitle": "DigiPassword Generator",
+  "digipassSiteHint": "Domeniul site-ului",
+  "digipassInstructionAbove": "Introduce domeniul mai sus, iar după care PIN-ul:",
+  "digipassInstructionBelow": "Introduce domeniul mai jos, iar după care PIN-ul:",
+  "digipassNote":
+      "*Bazându-ne pe seed-ul contului tău și datele introduse mai sus, o să fie generată o parola unică. Dacă data viitoare vei insera același date, o să fie generată aceeași parolă.",
+  "digipassPasswordCopy": "Parola ta (apasă pentru a copia):",
+
+  "warningInvalidChecksum": "Checksum invalid!  Ești sigur că este corect?",
+  "warningShortMnemonic1": "Doar propoziția mnemonic ",
+//$enteredBitLength
+  "warningShortMnemonic2":
+      " bits. Propozițiile mnemonice mai scurte de 128bits nu sunt recomandate.",
+
+  "errorNoCamera": "Fără permisiuni de access la camera foto!",
+  "errorUnknown": "Eroare necunoscută: ",
+  "errorInvalidPin": "PIN invalid",
+  "errorNoConnection": "Nicio conexiune",
+  "errorNotDigiIDQR": "Nu este un DigiID QR",
+
+  "bioSettingsButton": "Du-te la setări",
+  "bioSettingsDescriptionAndroid": "Amprentele nu sunt setate pe acest dispozitiv. Navighează la 'Setări > Securitate' pentru a adăuga amprentele tale.",
+  "bioSettingsDescriptionApple": "Este necesar să activezi Touch ID",
+  "bioLockOutTitle":  "Eroare cu amprenta",
+  "bioAuthenticateTitle": "Folosește amprenta",
+  "bioAuthenticateDescription": "Trebuie să te autentifici",
+  "bioHint": "Senzor de atingere",
+  "bioSuccess": "Amprentă recunoscută",
+  "bioFail": "Amprentă necunoscută.  Încearcă din nou.",
+
+  "_Back": "Înapoi",
+  "_Submit": "Trimite",
+  "_Cancel": "Anulează",
+  "_Verify": "Verifică",
+  "_Warning": "Atenție",
+  "_Error": "Eroare",
+  "_Yes": "Da",
+  "_No": "Nu",
+  "_UseAnyways": "Folosește oricând",
+  "_Close": "Închide",
+  "_Success": "Succes",
+  "_Failed": "Eșuat",
+  "_Copy": "Copiază",
+};

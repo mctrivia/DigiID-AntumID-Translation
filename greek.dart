16d3e439b776f26a804df9f34d0cbb702112a5ef
gfkacid <gfkacid@gmail.com>
2019-03-19 14:05:28 +0200

    Create greek.dart
    greek translation added

---
+++ greek.dart
@@ -0,0 +1,82 @@
+final Map<String, String> greek = {
+  "language": "Greek",
+  "languageCode": "gr",
+  "languageMnemonic": "greek", //options: english,french,italian,spanish
+
+  "setupTitle": "Εγκατάσταση DigiID:",
+  "setupWarn":
+      "Είναι πολύ σημαντικό να βεβαιωθείτε πως είστε σε ασφαλή τοποθεσία, μακρυά από το οπτικό πεδίο καμερών ή άλλων ανρθώπων.",
+  "setupButtonCreate": "Δημιουργία Πορτοφολιού",
+  "setupButtonRecover": "Ανάκτηση Πορτοφολιού",
+
+  "setupCreateTitle": "Οδηγός Δημιουργίας Πορτοφολιού DigiID",
+  "setupCreateWarn":
+      "Γράψτε σε χαρτί, ή σκαλίστε σε μέταλο ή άλλο υλικό.\nΜΗΝ αποθηκεύσετε ηλεκτρονικά:",
+  "setupCreateInstructions": "Επιλέξτε τις λέξεις με την σειρά που εμφανίστηκαν:",
+  "setupCreateButtonWaver":
+      "Έχω αποθηκεύσει με ασφάλεια την φράση seed, εκτός σύνδεσης και σε ασφαλές μέρος.",
+  "setupCreateFail": "Λάθος σειρά",
+  
+  "setupRestoreTitle": "Οδηγός Ανάκτησης Πορτοφολιού DigiID",
+  "setupRestoreInstructions": "Αντιγράψτε τη φράση seed εδώ:",
+
+  "setupSaveTitle": "Προστασία DigiID Pin",
+  "setupSaveSetPin": "Ορίστε κωδικό pin:",
+  "setupSaveSaving":  "Αποθήκευση Seed",
+  "setupSavePinNote": "*Άλλαξαν νούμερα για να αποφευχθεί παρακολούθηση των δαχτύλων.",
+
+  "settingTitle": "Ρυθμίσεις",
+  "settingAllowBiometric": "Έπιτρέπονται Βιομετρικά:",
+  "settingStyle": "Στυλ:",
+  "settingLanguage": "Γλώσσα:",
+
+  "digiidConfirmTitle": "Επιβεβαίωση τοποθεσίας",
+  "digiidConfirmInstructions": "Εισάγετε PIN αν η μπάρα διυθύνσεων είπε ",
+  "digiidConfirmWaiting": "Διαπραγμάτευση με τοποθεσία",
+  "digiidConfirmBackupInstructions":
+      "Αυτός είναι ο κωδικός ανάκτησής σας.\nΜην χρησιμοποιήσετε κενά.\n\n",
+
+  "digipassTitle": "Δημιουργός DigiPassword",
+  "digipassSiteHint": "Τοποθεσία",
+  "digipassInstructionAbove": "Εισάγετε την τοποθεσία από πάνω, και στη συνέχεια τον κωδικό pin:",
+  "digipassInstructionBelow": "Εισάγετε την τοποθεσία από κάτω, και στη συνέχεια τον κωδικό pin:",
+  "digipassNote":
+      "*Βάση στο seed του λογαριασμού σας και την πληροφορία που εισάγετε παραπάνω, θα παραχθεί ένας μοναδικός κωδικός για εσάς. Εάν εισάγετε την ίδια λέξη ή φράση επόμενη φορά, θα παραχθεί ο ίδιος κωδικός.",
+  "digipassPasswordCopy": "Ο κωδικός σας(πατήστε για αντιγραφή):",
+
+  "warningInvalidChecksum": "Μη έγκυρο άρθροισμα ελέγχου (checksum)!  Είστε σίγουρος πως είναι σωστό?",
+  "warningShortMnemonic1": "Μόνο μνημονική φράση ",
+//$enteredBitLength
+  "warningShortMnemonic2":
+      " bits. Δεν συνιστάται χρήση μνημονικής φράσης μικρότερου μήκους από 128 bit.",
+
+  "errorNoCamera": "Απουσία εξιουσιοδότησης κάμερας!",
+  "errorUnknown": "Άγνωστο σφάλμα: ",
+  "errorInvalidPin": "Μη έγκυρο Pin",
+  "errorNoConnection": "Χωρίς σύνδεση",
+  "errorNotDigiIDQR": "QR μη συμβατό με DigiID ",
+
+  "bioSettingsButton": "Πήγαινε στις Ρυθμίσεις",
+  "bioSettingsDescriptionAndroid": "Η συσκευή δεν έχει ρυθμιστεί για υποστήριξη δακτυλικού αποτυπώματος. Μεταβείτε στις 'Ρυθμίσεις > Ασφάλεια' για να προσθέσετε το δακτυλικό σας αποτύπωμα.",
+  "bioSettingsDescriptionApple": "Παρακαλώ ενεργοποιήστε το Touch ID",
+  "bioLockOutTitle":  "Σφάλμα Δακτυλικού Αποτυπώματος",
+  "bioAuthenticateTitle": "Απαιτείται Δακτυλικό Αποτύπωμα",
+  "bioAuthenticateDescription": "Παρακαλώ ταυτοποιήστε",
+  "bioHint": "Αισθητήρας αφής",
+  "bioSuccess": "Δακτυλικό αποτύπωμα αναγνωρίστηκε",
+  "bioFail": "Δακτυλικό αποτύπωμα ΔΕΝ αναγνωρίστηκε.  Προσπαθήστε ξανά.",
+
+  "_Back": "Πίσω",
+  "_Submit": "Υποβολή",
+  "_Cancel": "Ακύρωση",
+  "_Verify": "Επιβεβαίωση",
+  "_Warning": "Προειδοποίηση",
+  "_Error": "Σφάλμα",
+  "_Yes": "Ναι",
+  "_No": "Όχι",
+  "_UseAnyways": "Θέλετε να προχωρήσετε",
+  "_Close": "Κλείσιμο",
+  "_Success": "Επιτυχία",
+  "_Failed": "Αποτυχία",
+  "_Copy": "Αντιγραφή",
+};

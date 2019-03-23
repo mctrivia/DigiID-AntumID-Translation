1f97fed3d297d99b24c55e8ddef44020bb7a2eb1
gfkacid <40727787+gfkacid@users.noreply.github.com>
2019-03-23 20:10:59 +0200

    Fixed language param + 2 more keys (#6)
    * Create greek.dart
    
    greek translation added
    
    * Update greek.dart
    
    fixed languageMnemonic
    
    * Update greek.dart
    
    fixed language param
    
    * Update greek.dart
    
    added version
    fixed double entry

--- greek.dart
+++ greek.dart
@@ -4,17 +4,20 @@ final Map<String, String> greek = {
   "languageMnemonic": "english", //options: english,french,italian,spanish
 
   "setupTitle": "Εγκατάσταση DigiID:",
-  "setupWarn": "Είναι πολύ σημαντικό να βεβαιωθείτε πως είστε σε ασφαλή τοποθεσία, μακρυά από το οπτικό πεδίο καμερών ή άλλων ανρθώπων.",
+  "setupWarn":
+      "Είναι πολύ σημαντικό να βεβαιωθείτε πως είστε σε ασφαλή τοποθεσία, μακρυά από το οπτικό πεδίο καμερών ή άλλων ανρθώπων.",
   "setupButtonCreate": "Δημιουργία Πορτοφολιού",
   "setupButtonRecover": "Ανάκτηση Πορτοφολιού",
 
   "setupCreateTitle": "Οδηγός Δημιουργίας Πορτοφολιού DigiID",
-  "setupCreateWarn": "Γράψτε σε χαρτί, ή σκαλίστε σε μέταλο ή άλλο υλικό.\nΜΗΝ αποθηκεύσετε ηλεκτρονικά:",
-  "setupCreateTemplate": "Κάντε λήψη προτύπου", //auto translate
+  "setupCreateWarn":
+      "Γράψτε σε χαρτί, ή σκαλίστε σε μέταλο ή άλλο υλικό.\nΜΗΝ αποθηκεύσετε ηλεκτρονικά:",
+  "setupCreateTemplate": "Λήψη Προτύπου",
   "setupCreateInstructions": "Επιλέξτε τις λέξεις με την σειρά που εμφανίστηκαν:",
-  "setupCreateButtonWaver": "Έχω αποθηκεύσει με ασφάλεια την φράση seed, εκτός σύνδεσης και σε ασφαλές μέρος.",
+  "setupCreateButtonWaver":
+      "Έχω αποθηκεύσει με ασφάλεια την φράση seed, εκτός σύνδεσης και σε ασφαλές μέρος.",
   "setupCreateFail": "Λάθος σειρά",
-
+  
   "setupRestoreTitle": "Οδηγός Ανάκτησης Πορτοφολιού DigiID",
   "setupRestoreInstructions": "Αντιγράψτε τη φράση seed εδώ:",
 
@@ -27,23 +30,26 @@ final Map<String, String> greek = {
   "settingAllowBiometric": "Έπιτρέπονται Βιομετρικά:",
   "settingStyle": "Στυλ:",
   "settingLanguage": "Γλώσσα:",
+  "settingVersion":"Έκδοση:",
 
   "digiidConfirmTitle": "Επιβεβαίωση τοποθεσίας",
   "digiidConfirmInstructions": "Εισάγετε PIN αν η μπάρα διυθύνσεων είπε ",
   "digiidConfirmWaiting": "Διαπραγμάτευση με τοποθεσία",
-  "digiidConfirmBackupInstructions": "Αυτός είναι ο κωδικός ανάκτησής σας.\nΜην χρησιμοποιήσετε κενά.\n\n",
+  "digiidConfirmBackupInstructions":
+      "Αυτός είναι ο κωδικός ανάκτησής σας.\nΜην χρησιμοποιήσετε κενά.\n\n",
 
   "digipassTitle": "Δημιουργός DigiPassword",
   "digipassSiteHint": "Τοποθεσία",
-  "digipassInstructionAbove": "Εισάγετε την τοποθεσία από πάνω, και στη συνέχεια τον κωδικό pin:",
   "digipassInstructionBelow": "Εισάγετε την τοποθεσία από κάτω, και στη συνέχεια τον κωδικό pin:",
-  "digipassNote": "*Βάση στο seed του λογαριασμού σας και την πληροφορία που εισάγετε παραπάνω, θα παραχθεί ένας μοναδικός κωδικός για εσάς. Εάν εισάγετε την ίδια λέξη ή φράση επόμενη φορά, θα παραχθεί ο ίδιος κωδικός.",
+  "digipassNote":
+      "*Βάση στο seed του λογαριασμού σας και την πληροφορία που εισάγετε παραπάνω, θα παραχθεί ένας μοναδικός κωδικός για εσάς. Εάν εισάγετε την ίδια λέξη ή φράση επόμενη φορά, θα παραχθεί ο ίδιος κωδικός.",
   "digipassPasswordCopy": "Ο κωδικός σας(πατήστε για αντιγραφή):",
 
   "warningInvalidChecksum": "Μη έγκυρο άρθροισμα ελέγχου (checksum)!  Είστε σίγουρος πως είναι σωστό?",
-  "warningShortMnemonic1": "Μόνο μνημονική φράση ",
+  "warningShortMnemonic1": "Μήκος μνημονικής φράσης μόνο",
 //$enteredBitLength
-  "warningShortMnemonic2": " bits. Δεν συνιστάται χρήση μνημονικής φράσης μικρότερου μήκους από 128 bit.",
+  "warningShortMnemonic2":
+      " bits. Δεν συνιστάται χρήση μνημονικής φράσης μικρότερου μήκους από 128 bit.",
 
   "errorNoCamera": "Απουσία εξιουσιοδότησης κάμερας!",
   "errorUnknown": "Άγνωστο σφάλμα: ",
@@ -69,7 +75,7 @@ final Map<String, String> greek = {
   "_Error": "Σφάλμα",
   "_Yes": "Ναι",
   "_No": "Όχι",
-  "_UseAnyways": "Θέλετε να προχωρήσετε",
+  "_UseAnyways": "Χρήση πάραυτα",
   "_Close": "Κλείσιμο",
   "_Success": "Επιτυχία",
   "_Failed": "Αποτυχία",

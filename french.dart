06cc802b05b38ff9c177070b3713df2592ab72f4
Matthew Cornelisse <squarerootofnegativeone@gmail.com>
2019-03-28 06:51:25 -0500

    updated by Cedric

--- french.dart
+++ french.dart
@@ -3,84 +3,84 @@ final Map<String, String> french = {
   "languageCode": "fr",
   "languageMnemonic": "french", //options: english,french,italian,spanish
 
-  "setupTitle": "Configuration DigiID:",	//auto tranlasted
-  "setupWarn": "Il est très important de vous assurer que vous êtes dans un endroit sécurisé sans caméra ni personne qui vous surveille avant de continuer.",	//auto tranlasted
-  "setupButtonCreate": "Créer un Nouveau Portefeuille",	//auto tranlasted
-  "setupButtonRecover": "Récupérer le Portefeuille",	//auto tranlasted
+  "setupTitle": "Configuration de DigiID:",
+  "setupWarn": "Il est très important de vous assurer que vous êtes dans un endroit sécurisé, sans caméra et à l abris de tout regards, avant de continuer.",
+  "setupButtonCreate": "Créer un Nouveau Portefeuille",
+  "setupButtonRecover": "Récupérer un Portefeuille",
 
-  "setupCreateTitle": "Créateur de Portefeuille DigiID",	//auto tranlasted
-  "setupCreateWarn": "Écrivez sur du papier ou percez du métal.\nNe pas stocker électroniquement:",	//auto tranlasted
-  "setupCreateTemplate": "Télécharger le modèle",	//auto tranlasted
-  "setupCreateInstructions": "Cliquez sur les mots dans l'ordre où ils sont apparus:",	//auto tranlasted
-  "setupCreateButtonWaver": "J'ai stocké ma phrase de départ en toute sécurité dans un endroit sûr",	//auto tranlasted
-  "setupCreateFail": "Mauvais Ordre",	//auto tranlasted
+  "setupCreateTitle": "Créateur de Portefeuille DigiID",
+  "setupCreateWarn": "Écrivez sur du papier ou graver sur du métal.\nNe pas sauvegarder électroniquement:",
+  "setupCreateTemplate": "Télécharger le modèle",
+  "setupCreateInstructions": "Cliquez sur les mots dans l'ordre de leur apparition:",
+  "setupCreateButtonWaver": "J'ai sauvegarde en toute sécurité ma phrase dans un endroit sûr et hors ligne",
+  "setupCreateFail": "Mauvais Ordre",
 
-  "setupRestoreTitle": "Portefeuille Restaurateur DigiID",	//auto tranlasted
-  "setupRestoreInstructions": "Copier la phrase mnemoni ici:",	//auto tranlasted
+  "setupRestoreTitle": "Restaurateur de Portefeuille DigiID",
+  "setupRestoreInstructions": "Copier la phrase mnémonique ici:",
 
-  "setupSaveTitle": "DigiID Pin Protect",	//auto tranlasted
-  "setupSaveSetPin": "Placez une épingle:",	//auto tranlasted
-  "setupSaveSaving":  "Sauver des semences",	//auto tranlasted
-  "setupSavePinNote": "*Les chiffres ont été modifiés pour éviter le suivi des doigts.",	//auto tranlasted
+  "setupSaveTitle": "Proteger DigiID avec un code secret",
+  "setupSaveSetPin": "Créer votre code secret:",
+  "setupSaveSaving":  "Sauvegarde des seeds",
+  "setupSavePinNote": "*Les chiffres ont été déplacés pour éviter le trackage de vos doigts.",
 
-  "settingTitle": "Réglages",	//auto tranlasted
-  "settingAllowBiometric": "Autoriser biométrique:",	//auto tranlasted
-  "settingStyle": "Style(Reboot Required):",	//auto tranlasted
-  "settingLanguage": "La langue:",	//auto tranlasted
-  "settingVersion": "Version:",			//left english
+  "settingTitle": "Réglages",
+  "settingAllowBiometric": "Autoriser biométrique:",
+  "settingStyle": "Style (Redémarrage nécessaire):",
+  "settingLanguage": "Langue:",
+  "settingVersion": "Version:",
 
-  "digiidConfirmTitle": "Confirmer le Domaine",	//auto tranlasted
-  "digiidConfirmInstructions": "Entrez NIP si la barre d’adresse a déclaré ",	//auto tranlasted
-  "digiidConfirmWaiting": "Négociation avec le site",	//auto tranlasted
-  "digiidConfirmBackupInstructions": "Ceci est votre mot de passe de sauvegarde.\nNe pas entrer dans les espaces.\n\n",	//auto tranlasted
+  "digiidConfirmTitle": "Confirmer le Domaine",
+  "digiidConfirmInstructions": "Entrez votre code secret uniquement si la barre d’adresse est la même que ",
+  "digiidConfirmWaiting": "Négociation avec le site",
+  "digiidConfirmBackupInstructions": "Ceci est votre mot de passe de sauvegarde.\nNe pas entrer les espaces.\n\n",
 
-  "digipassTitle": "Générateur DigiPassword",	//auto tranlasted
-  "digipassSiteHint": "Domaine du Site",	//auto tranlasted
-  "digipassInstructionAbove": "Entrez le domaine ci-dessus, puis entrez le code PIN:",	//auto tranlasted
-  "digipassInstructionBelow": "Entrez le domaine ci-dessous, puis entrez le code PIN:",	//auto tranlasted
-  "digipassNote": "*En fonction de la valeur de départ de votre compte et de l'entrée ci-dessus, cela générera un mot de passe unique pour vous. Si vous entrez le même mot ou la même phrase une seconde fois, le même mot de passe sera généré.",	//auto tranlasted
-  "digipassPasswordCopy": "Votre mot de passe (tapez pour copier):",	//auto tranlasted
-  "digipassSendToWidget1": "Envoyer à ",	//auto tranlasted
+  "digipassTitle": "Générateur de DigiPassword",
+  "digipassSiteHint": "Domaine du Site",
+  "digipassInstructionAbove": "Entrez le domaine ci-dessus, puis entrez votre code secret:",
+  "digipassInstructionBelow": "Entrez le domaine ci-dessous, puis entrez votre code secret:",
+  "digipassNote": "*En couplant votre compte personnel avec l'entrée ci-dessus, DigiPassword générera un mot de passe unique pour vous. Si vous entrez le même mot ou la même phrase une seconde fois, le même mot de passe sera généré.",
+  "digipassPasswordCopy": "Votre mot de passe (tapez pour copier):",
+  "digipassSendToWidget1": "Envoyer à ",
 //$widgetDomain
-  "digipassSendToWidget2": " widget",	//auto tranlasted
-  "digipassSendingToWidget1": "Envoi d'un mot de passe crypté à ",	//auto tranlasted
+  "digipassSendToWidget2": " widget",
+  "digipassSendingToWidget1": "Envoi d'un mot de passe crypté à ",
 //$widgetDomain
-  "digipassSendingToWidget2": " widget",	//auto tranlasted
-  
-  "warningInvalidChecksum": "Somme de contrôle invalide! Êtes-vous sûr que c'est correct?",	//auto tranlasted
-  "warningShortMnemonic1": "Phrase mnémonique seulement ",	//auto tranlasted
+  "digipassSendingToWidget2": " widget",
+
+  "warningInvalidChecksum": "Somme de contrôle invalide! Êtes-vous sûr que c'est correct?",
+  "warningShortMnemonic1": "Phrase mnémonique uniquement ",
 //$enteredBitLength
-  "warningShortMnemonic2": " bits. Les phrases mnémoniques plus courtes que 128 bits ne sont pas recommandées.",	//auto tranlasted
+  "warningShortMnemonic2": " bits. Les phrases mnémoniques plus courtes que 128 bits ne sont pas recommandées.",
 
-  "errorNoCamera": "Aucune autorisation de caméra!",	//auto tranlasted
-  "errorUnknown": "Erreur inconnue: ",	//auto tranlasted
-  "errorInvalidPin": "Pin invalide",	//auto tranlasted
-  "errorNoConnection": "Pas de Connection",	//auto tranlasted
-  "errorNotDigiIDQR": "Pas DigiID QR",	//auto tranlasted
+  "errorNoCamera": "Aucune autorisation de caméra!",
+  "errorUnknown": "Erreur inconnue: ",
+  "errorInvalidPin": "Code secret invalide",
+  "errorNoConnection": "Pas de Connection",
+  "errorNotDigiIDQR": "Pas de DigiID QR",
 
-  "bioSettingsButton": "Aller aux paramètres",	//auto tranlasted
-  "bioSettingsDescriptionAndroid": "L'empreinte digitale n'est pas configurée sur votre appareil. Allez dans 'Paramètres> Sécurité' pour ajouter votre empreinte digitale.",	//auto tranlasted
-  "bioSettingsDescriptionApple": "Veuillez activer votre Touch ID",	//auto tranlasted
-  "bioLockOutTitle":  "Erreur d'empreinte digitale",	//auto tranlasted
-  "bioAuthenticateTitle": "Empreinte digitale requise",	//auto tranlasted
-  "bioAuthenticateDescription": "S'il vous plaît authentifier",	//auto tranlasted
-  "bioHint": "Toucher le capteur",	//auto tranlasted
-  "bioSuccess": "Empreinte digitale reconnue",	//auto tranlasted
-  "bioFail": "Empreinte digitale non reconnue. Réessayer.",	//auto tranlasted
+  "bioSettingsButton": "Retour aux paramètres",
+  "bioSettingsDescriptionAndroid": "L'empreinte digitale n'est pas configurée sur votre appareil. Allez dans 'Paramètres> Sécurité' pour ajouter votre empreinte digitale.",
+  "bioSettingsDescriptionApple": "Veuillez activer votre Touch ID",
+  "bioLockOutTitle":  "Erreur d'empreinte digitale",
+  "bioAuthenticateTitle": "Empreinte digitale requise",
+  "bioAuthenticateDescription": "S'il vous plaît authentifier",
+  "bioHint": "Toucher le capteur",
+  "bioSuccess": "Empreinte digitale reconnue",
+  "bioFail": "Empreinte digitale non reconnue. Réessayer.",
 
-  "alertCopyClip": "Copié dans le presse-papier",	//auto tranlasted
-  
-  "_Back": "Retour",	//auto tranlasted
-  "_Submit": "Soumission",	//auto tranlasted
-  "_Cancel": "Annuler",	//auto tranlasted
-  "_Verify": "Vérifier",	//auto tranlasted
-  "_Warning": "Attention",	//auto tranlasted
-  "_Error": "Erreur",	//auto tranlasted
-  "_Yes": "Oui",	//auto tranlasted
-  "_No": "Non",	//auto tranlasted
-  "_UseAnyways": "Utilisez quand même",	//auto tranlasted
-  "_Close": "Fermer",	//auto tranlasted
-  "_Success": "Succès",	//auto tranlasted
-  "_Failed": "Échoué",	//auto tranlasted
-  "_Copy": "Copie",	//auto tranlasted
+  "alertCopyClip": "Copié dans le presse-papier",
+
+  "_Back": "Retour",
+  "_Submit": "Soumettre",
+  "_Cancel": "Annuler",
+  "_Verify": "Vérifier",
+  "_Warning": "Attention",
+  "_Error": "Erreur",
+  "_Yes": "Oui",
+  "_No": "Non",
+  "_UseAnyways": "Utilisez quand même",
+  "_Close": "Fermer",
+  "_Success": "Succès",
+  "_Failed": "Échoué",
+  "_Copy": "Copie",
 };

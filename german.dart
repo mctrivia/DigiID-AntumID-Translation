7fe1fe51e810a72616e72ae548effb1c9600f90a
Matthew Cornelisse <squarerootofnegativeone@gmail.com>
2019-04-24 19:56:53 -0500

    uploaded submition by @Franke247de

---
+++ german.dart
@@ -0,0 +1,86 @@
+final Map<String, String> german = {
+  "language": "Deutsch",
+  "languageCode": "de",
+  "languageMnemonic": "english", //options: english,french,italian,spanish
+
+  "setupTitle": "Digi-ID Einrichtung:",
+  "setupWarn": "Es ist sehr wichtig, dass du dich vergewisserst an einem sicheren Ort ohne Kameras oder anderen Personen, die dir zusehen könnten, zu sein, bevor du weitermachst.",
+  "setupButtonCreate": "Neue Wallet erstellen",
+  "setupButtonRecover": "Wallet wiederherstellen",
+
+  "setupCreateTitle": "Digi-ID Wallet Erstellung",
+  "setupCreateWarn": "Schreibe es auf Papier oder schlag es in Metall.\nSpeicher es aber nicht elektronisch:",
+  "setupCreateTemplate": "Vorlage herunterladen",
+  "setupCreateInstructions": "Klicke die Wörter in der Reihenfolge, wie sie erschienen, an:",
+  "setupCreateButtonWaver": "Ich habe die Seed Phrase offline und an einem sicheren Ort gespeichert",
+  "setupCreateFail": "Falsche Reihenfolge",
+
+  "setupRestoreTitle": "Digi-ID Wallet Wiederherstellung",
+  "setupRestoreInstructions": "Füge die mnemonische Phrase hier ein:",
+
+  "setupSaveTitle": "Digi-ID PIN Schutz",
+  "setupSaveSetPin": "Lege eine PIN fest:",
+  "setupSaveSaving":  "Speichere Seed",
+  "setupSavePinNote": "*Ziffern getauscht, um Finger-Tracking zu verhindern.",
+
+  "settingTitle": "Einstellungen",
+  "settingAllowBiometric": "Erlaube Biometrie:",
+  "settingStyle": "Stil:",
+  "settingLanguage": "Sprache:",
+  "settingVersion": "Version:",
+
+  "digiidConfirmTitle": "Domain bestätigen",
+  "digiidConfirmInstructions": "Gebe deine PIN ein, wenn in der Adresszeile stand: ",
+  "digiidConfirmWaiting": "Validierung mit der Website",
+  "digiidConfirmBackupInstructions": "Das ist dein Backup-Passwort.\nGebe keine Leerzeichen ein.\n\n",
+
+  "digipassTitle": "DigiPassword Generator",
+  "digipassSiteHint": "Website Domain",
+  "digipassInstructionAbove": "Gebe oben die Domain ein, dann deine PIN:",
+  "digipassInstructionBelow": "Gebe unten die Domain ein, dann deine PIN:",
+  "digipassNote": "*Basierend auf deinem Konto-Seed und der obigen Eingabe, wird ein einzigartiges Passwort für dich generiert. Wenn du dasselbe Wort oder dieselbe Phrase ein zweites Mal eingibst, wird auch dasselbe Passwort generiert.",
+  "digipassPasswordCopy": "Dein Passwort (anklicken zum Kopieren):",
+  "digipassSendToWidget1": "Zum ",
+//$widgetDomain
+  "digipassSendToWidget2": " Widget senden",
+  "digipassSendingToWidget1": "Sende das verschlüsselte Passwort zum ",
+//$widgetDomain
+  "digipassSendingToWidget2": " Widget",
+
+  "warningInvalidChecksum": "Ungültige Prüfsumme! Bist du sicher, dass das korrekt ist?",
+  "warningShortMnemonic1": "Mnemonische Phrase hat nur ",
+//$enteredBitLength
+  "warningShortMnemonic2": " Bits. Mnemonische Phrasen kürzer als 128 Bits werden nicht empfohlen.",
+
+  "errorNoCamera": "Keine Kamera-Berechtigung!",
+  "errorUnknown": "Unbekannter Fehler: ",
+  "errorInvalidPin": "Ungültige PIN",
+  "errorNoConnection": "Keine Verbindung",
+  "errorNotDigiIDQR": "Kein Digi-ID QR-Code",
+
+  "bioSettingsButton": "Zu den Einstellungen",
+  "bioSettingsDescriptionAndroid": "Es ist kein Fingerabdruck auf deinem Gerät eingerichtet. Gehe zu 'Einstellungen > Sicherheit', um deinen Fingerabdruck hinzuzufügen.",
+  "bioSettingsDescriptionApple": "Bitte aktiviere Touch ID",
+  "bioLockOutTitle":  "Fingerabdruck Fehler",
+  "bioAuthenticateTitle": "Fingerabdruck benötigt",
+  "bioAuthenticateDescription": "Bitte authentifiziere dich",
+  "bioHint": "Fingerabdruck-Sensor",
+  "bioSuccess": "Fingerabdruck erkannt",
+  "bioFail": "Fingerabdruck nicht erkannt. Versuch es nochmal.",
+
+  "alertCopyClip": "In die Zwischenablage kopiert",
+
+  "_Back": "Zurück",
+  "_Submit": "Senden",
+  "_Cancel": "Abbrechen",
+  "_Verify": "Überprüfen",
+  "_Warning": "Warnung",
+  "_Error": "Fehler",
+  "_Yes": "Ja",
+  "_No": "Nein",
+  "_UseAnyways": "Trotzdem verwenden",
+  "_Close": "Schließen",
+  "_Success": "Erfolgreich",
+  "_Failed": "Fehlgeschlagen",
+  "_Copy": "Kopieren",
+};
